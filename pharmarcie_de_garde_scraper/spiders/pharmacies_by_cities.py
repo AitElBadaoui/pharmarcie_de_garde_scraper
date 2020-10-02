3e2252c8d0cf54a85863b793307c6a781a063c9e
Mouad <mouad.aitelbad@gmail.com>
2020-10-02 16:19:44 +0200

    :triangular_flag_on_post:: scrap home page

--- pharmarcie_de_garde_scraper/spiders/pharmacies_by_cities.py
+++ pharmarcie_de_garde_scraper/spiders/pharmacies_by_cities.py
@@ -4,7 +4,13 @@ import scrapy
 class PharmaciesByCitiesSpider(scrapy.Spider):
     name = 'pharmacies_by_cities'
     allowed_domains = ['https://www.annuaire-gratuit.ma/pharmacie-garde-maroc.html']
-    start_urls = ['http://https://www.annuaire-gratuit.ma/pharmacie-garde-maroc.html/']
-
+    start_urls = ['https://www.annuaire-gratuit.ma/pharmacie-garde-maroc.html']
     def parse(self, response):
-        pass
+        index = 0
+        for city in response.css('article'):
+            index +=1
+            if city.css('h3 a::text').get():
+                yield {
+                    'id':index, 'city_fr': city.css('h3 a::text').get().strip() , 'city_ar': city.css('h3> a > span::text').get().strip(), 'number': city.css('p::text').get().strip()
+                    }
+        
